ab21ad13ee252f621b7bfd5f516ea4c06ebd067d
ali11reza11 <130246122+ali11reza11@users.noreply.github.com>
2023-08-08 14:54:44 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".btn");
+const container = document.querySelector(".container");
+
+button.addEventListener("click" , ()=>{
+    container.classList.toggle("active");
+})
+
